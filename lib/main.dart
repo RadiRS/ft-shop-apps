5be6b403ba3ea76c4eb72724bb8f4f3d12ad2e7e
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-05-20 13:08:14 +0700

    Added product_item widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview.screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Text('Initial Project'),
+      home: ProductsOverviewScreen(),
     );
   }
 }

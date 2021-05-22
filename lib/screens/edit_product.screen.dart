44fd497fcffafe7b39d2ffdc1f2103f70ad0784d
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-05-22 18:07:26 +0700

    Added multiple TextFormField & added focused listeners

--- lib/screens/edit_product.screen.dart
+++ lib/screens/edit_product.screen.dart
@@ -10,6 +10,16 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  // Added focus listeners to the input
+  final _priceFocuseNode = FocusNode();
+
+  @override
+  void dispose() {
+    // Clear the listeners to avoid memory leak
+    _priceFocuseNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +34,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  // Set focus the next form input (manual step)
+                  FocusScope.of(context).requestFocus(_priceFocuseNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocuseNode,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
               ),
             ],
           ),

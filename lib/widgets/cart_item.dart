76b470c2e5bc04c1c70083c1dcc15ff7734b94f1
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-05-22 17:15:45 +0700

    Added Alert Dialogs widget to confirm at cart_item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -26,6 +26,31 @@ class CartItem extends StatelessWidget {
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
       onDismissed: (direction) => cart.removeItem(productId),
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (_) {
+              return AlertDialog(
+                title: const Text('Are you sure?'),
+                content:
+                    const Text('Do you want to remove the item from the cart?'),
+                actions: [
+                  TextButton(
+                    child: const Text('No'),
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Yes'),
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                  ),
+                ],
+              );
+            });
+      },
       background: Container(
         color: Theme.of(context).errorColor,
         child: const Icon(

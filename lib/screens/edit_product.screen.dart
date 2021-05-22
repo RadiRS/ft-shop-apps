9650033d1af96f2ccdaa0e28dd7a98a6055a4ef8
Radi Rusadi <radi.rusadi.s@gmail.com>
2021-05-22 21:53:52 +0700

    Added form validation input

--- lib/screens/edit_product.screen.dart
+++ lib/screens/edit_product.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/product.dart';
 
 class EditProductScreen extends StatefulWidget {
   static String routeName = '/edit-product';
@@ -14,6 +15,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocuseNode = FocusNode();
   final _imageUrlFocusNode = FocusNode();
   final _imageUrlController = TextEditingController();
+  // Set global key form to access the state of Form widget
+  final _form = GlobalKey<FormState>();
+
+  Product _editedProduct = Product(
+    id: null,
+    title: '',
+    description: '',
+    price: 0,
+    imageUrl: '',
+  );
 
   @override
   void initState() {
@@ -25,15 +36,39 @@ class _EditProductScreenState extends State<EditProductScreen> {
   @override
   void dispose() {
     // Clear the listeners to avoid memory leak
+    _imageUrlController.removeListener(_updateImageUrl);
     _priceFocuseNode.dispose();
     _imageUrlFocusNode.dispose();
     _imageUrlController.dispose();
-    _imageUrlController.removeListener(_updateImageUrl);
     super.dispose();
   }
 
   void _updateImageUrl() {
-    if (!_imageUrlFocusNode.hasFocus) setState(() {});
+    if (!_imageUrlFocusNode.hasFocus) {
+      var urlPattern =
+          r"(https?|ftp)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
+      var result = new RegExp(urlPattern, caseSensitive: false)
+          .hasMatch(_imageUrlController.text);
+
+      if (!result ||
+          (!_imageUrlController.text.endsWith('.png') &&
+              !_imageUrlController.text.endsWith('.jpg') &&
+              !_imageUrlController.text.endsWith('.jpeg'))) return;
+
+      setState(() {});
+    }
+  }
+
+  void _saveForm() {
+    // Dismiss soft keyboard
+    FocusScope.of(context).unfocus();
+
+    // Validate the form and is valid or not
+    final isValid = _form.currentState.validate();
+
+    if (!isValid) return;
+
+    _form.currentState.save();
   }
 
   @override
@@ -41,38 +76,99 @@ class _EditProductScreenState extends State<EditProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit Product'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: _saveForm,
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
+          autovalidateMode: AutovalidateMode.always,
           child: ListView(
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Title'),
+                decoration: const InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
+                textCapitalization: TextCapitalization.words,
                 onFieldSubmitted: (_) {
                   // Set focus the next form input (manual step)
                   FocusScope.of(context).requestFocus(_priceFocuseNode);
                 },
+                validator: (value) {
+                  if (value.isEmpty) return 'Please enter a title.';
+
+                  return null;
+                },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: null,
+                    title: value,
+                    description: _editedProduct.description,
+                    price: _editedProduct.price,
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Price'),
+                decoration: const InputDecoration(labelText: 'Price'),
                 textInputAction: TextInputAction.next,
-                keyboardType: TextInputType.number,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
                 focusNode: _priceFocuseNode,
+                validator: (value) {
+                  if (value.isEmpty) return 'Please enter a price';
+
+                  if (double.tryParse(value) == null)
+                    return 'Please enter a valid number';
+
+                  if (double.parse(value) <= 0)
+                    return 'Please enter a number greater than zero';
+
+                  return null;
+                },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: null,
+                    title: _editedProduct.title,
+                    description: _editedProduct.description,
+                    price: double.parse(value),
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'Description'),
+                decoration: const InputDecoration(labelText: 'Description'),
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+                validator: (value) {
+                  if (value.isEmpty) return 'Please enter a description';
+
+                  if (value.length < 10)
+                    return 'Should at lest 10 characters long';
+
+                  return null;
+                },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: null,
+                    title: _editedProduct.title,
+                    description: value,
+                    price: _editedProduct.price,
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
+              SizedBox(height: 20),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Container(
                     width: 100,
                     height: 100,
-                    margin: EdgeInsets.only(
+                    margin: const EdgeInsets.only(
                       top: 8,
                       right: 10,
                     ),
@@ -83,7 +179,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       ),
                     ),
                     child: _imageUrlController.text.isEmpty
-                        ? Text('Enter a URL')
+                        ? const Text('Enter a URL')
                         : FittedBox(
                             child: Image.network(
                               _imageUrlController.text,
@@ -93,15 +189,42 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   ),
                   Expanded(
                     child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Image URL'),
+                      decoration: const InputDecoration(labelText: 'Image URL'),
                       keyboardType: TextInputType.url,
                       textInputAction: TextInputAction.done,
                       controller: _imageUrlController,
                       focusNode: _imageUrlFocusNode,
+                      validator: (value) {
+                        if (value.isEmpty) return 'Please enter an image URL';
+
+                        var urlPattern =
+                            r"(https?|ftp)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
+                        var result =
+                            new RegExp(urlPattern, caseSensitive: false)
+                                .hasMatch(value);
+
+                        if (!result) return 'Please enter a valid URL';
+
+                        if (!value.endsWith('.png') &&
+                            !value.endsWith('.jpg') &&
+                            !value.endsWith('.jpeg'))
+                          return 'Please enter a valid Image URL';
+
+                        return null;
+                      },
                       onEditingComplete: () {
-                        // Dismiss soft keyboard & rebuild the widget to display an image
-                        FocusScope.of(context).unfocus();
+                        // Rebuild the widget to display an image
                         setState(() {});
+                      },
+                      onFieldSubmitted: (_) => _saveForm(),
+                      onSaved: (value) {
+                        _editedProduct = Product(
+                          id: null,
+                          title: _editedProduct.title,
+                          description: _editedProduct.description,
+                          price: _editedProduct.price,
+                          imageUrl: value,
+                        );
                       },
                     ),
                   ),
